d5916817b019f5420e5e04a313b116e1b6b9e03a
ec2-docker-e433fa6f1b0b-amazonlinux2003 <ec2-docker-e433fa6f1b0b-amazonlinux2003@ba>
2024-07-10 13:10:14 +0000

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-export PATH="$PATH:/opt/mqm/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/mqm/lib:/opt/mqm/lib64"
+#export PATH="$PATH:/opt/mqm/bin"
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/mqm/lib:/opt/mqm/lib64"
+
+cd /opt/team4codebucket/MQServer
+#./mqlicense.sh
+
+for i in `echo 'MQSeriesRuntime-9.4.0-0.x86_64.rpm MQSeriesGSKit-9.4.0-0.x86_64.rpm MQSeriesSDK-9.4.0-0.x86_64.rpm MQSeriesClient-9.4.0-0.x86_64.rpm'`; do
+	echo "----------------------------------------"
+	echo $i
+	rpm -Kv $i
+	#rpm --prefix="/opt/team4codebucket/MQServer/mqm" -Uvh $i
+	rpm -Uvh $i
+done
