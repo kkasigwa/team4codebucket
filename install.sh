9f7ff0f7ebba3bf871b95103768530669db79fa2
Kyelema Kasigwa <kyelema.kasigwa@iaggbs.com>
2024-07-10 14:36:46 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 yum install -y ssh
+yum install -y git
+yum install -y nano vim vi
+
+yum install -y python3 python3-pip python3-devel
+yum install -y gcc
+#yum install -y 
+
+pip3 install virtualenv
+
