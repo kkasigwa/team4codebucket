4aad20138b450a13becd1f57860359fc555651b7
Kyelema Kasigwa <kyelema.kasigwa@iaggbs.com>
2024-07-10 14:52:24 +0100

    mod start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,19 @@
 #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/mqm/lib:/opt/mqm/lib64"
 
 cd /opt/team4codebucket/MQServer
-#./mqlicense.sh
+./mqlicense.sh -accept
 
-for i in `echo 'MQSeriesRuntime-9.4.0-0.x86_64.rpm MQSeriesGSKit-9.4.0-0.x86_64.rpm MQSeriesSDK-9.4.0-0.x86_64.rpm MQSeriesClient-9.4.0-0.x86_64.rpm'`; do
-	echo "----------------------------------------"
-	echo $i
-	rpm -Kv $i
-	#rpm --prefix="/opt/team4codebucket/MQServer/mqm" -Uvh $i
-	rpm -Uvh $i
-done
+#for i in `echo 'MQSeriesRuntime-9.4.0-0.x86_64.rpm MQSeriesGSKit-9.4.0-0.x86_64.rpm MQSeriesSDK-9.4.0-0.x86_64.rpm MQSeriesClient-9.4.0-0.x86_64.rpm'`; do
+#	echo "----------------------------------------"
+#	echo $i
+#	rpm -Kv $i
+#	#rpm --prefix="/opt/team4codebucket/MQServer/mqm" -Uvh $i
+#	rpm -Uvh $i
+#done
+
+rpm -Uvh MQSeriesRuntime-9.4.0-0.x86_64.rpm
+rpm -Uvh MQSeriesGSKit-9.4.0-0.x86_64.rpm
+rpm -Uvh MQSeriesSDK-9.4.0-0.x86_64.rpm
+rpm -Uvh MQSeriesClient-9.4.0-0.x86_64.rpm
+
+
