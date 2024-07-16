9d926037c2fdd2ec1e6add7977e2542e75a17df2
ec2-docker-30fab7403fd1-amazonlinux2003 <ec2-docker-30fab7403fd1-amazonlinux2003@ba>
2024-07-16 12:07:22 +0000

    refactor

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$PATH:/opt/mqm/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/mqm/lib:/opt/mqm/lib64"
+
--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export PATH="$PATH:/opt/mqm/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/mqm/lib:/opt/mqm/lib64"
+. ./env.sh
 
 cd /opt/team4codebucket/MQServer
 ./mqlicense.sh -accept
